57a330a535d186ee09316cae0fa276524f9d9c99
gamzenurtukel <gamzenurtukel@gmail.com>
2022-11-30 20:43:40 +0300

    header component created

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header>
+      <div className="header">
+        <h4>Weather App</h4>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
