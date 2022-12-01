c364e75dc6fcabf8ba648086d71eab69f3c81b0f
gamzenurtukel <gamzenurtukel@gmail.com>
2022-12-01 15:37:30 +0300

    weather component edited

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,7 +1,25 @@
 import "./Weather.scss";
+import { AiFillCloud } from "react-icons/ai";
 
 function Weather() {
-  return <div>weather</div>;
+  return (
+    <div className="weather">
+      <div className="weather-city">
+        <p>New York,US</p>
+        <span>wednesday 1 April</span>
+      </div>
+      <div className="weather-detail">
+        <div className="weather-temperature">
+          <AiFillCloud className="weather-icon" />
+          <div className="temperature-items">
+            <p className="degrees-value">5°</p>
+            <p className="weather-status">overcast clouds</p>
+          </div>
+        </div>
+        <div className="weather-temperature-other">ksksksk</div>
+      </div>
+    </div>
+  );
 }
 
 export default Weather;
