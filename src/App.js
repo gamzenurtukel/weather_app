7db009a2637d33ef07b14d97b1d2a047e2a01bf0
gamzenurtukel <gamzenurtukel@gmail.com>
2022-12-01 15:39:08 +0300

    items edited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.scss";
+import Header from "./components/header/Header";
+import SearchBar from "./components/searchBar/SearchBar";
+import Weather from "./components/weather";
 
 function App() {
-  return <div className="App">gamzeee</div>;
+  return (
+    <div>
+      <Header />
+      <div className="App">
+        <SearchBar />
+        <Weather />
+      </div>
+    </div>
+  );
 }
 
 export default App;
