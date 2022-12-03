22c3f13a22541683689762e1014e59fa16756317
gamzenurtukel <gamzenurtukel@gmail.com>
2022-12-03 19:58:01 +0300

    search functions created

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,15 +1,42 @@
 import "./SearchBar.scss";
 
 import { AiOutlineSearch } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { fetchAsyncWeather } from "../../redux/slices/weatherSlice";
+import { useDebounce } from "use-lodash-debounce";
 
 function SearchBar() {
+  const [inputValue, setInputValue] = useState("istanbul");
+  const dispatch = useDispatch();
+  const debouncedValue = useDebounce(inputValue, 1500);
+
+  useEffect(() => {
+    dispatch(fetchAsyncWeather(debouncedValue));
+  }, [dispatch, debouncedValue]);
+
+  const formOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const inputOnChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="search-bar">
       <div className="search-bar-container">
-        <i>
-          <AiOutlineSearch />
-        </i>
-        <input type="text" placeholder="Search City" />
+        <form onSubmit={formOnSubmit}>
+          <i>
+            <AiOutlineSearch />
+          </i>
+          <input
+            type="text"
+            placeholder="Search City"
+            value={inputValue}
+            onChange={inputOnChange}
+          />
+        </form>
       </div>
     </div>
   );
