53797c20f1c35890fceeac8189c4e42f4d03eee6
gamzenurtukel <gamzenurtukel@gmail.com>
2022-12-07 10:22:32 +0300

    redux test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import Header from "./components/header";
+import Weather from "./components/weather";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App/>", () => {
+  const initialState = {
+    weathers: {},
+  };
+  const middlewares = [thunk];
+  const mockStore = configureStore(middlewares);
+  let store;
+
+  test("app render", async () => {
+    store = mockStore(initialState);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(<Header />).toBeDefined();
+    expect(<Weather />).toBeDefined();
+  });
 });
